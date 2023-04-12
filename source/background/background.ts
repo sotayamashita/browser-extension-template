99598a586670549feb47014bc34a822c5c5fab48
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2023-04-12 21:54:58 +0900

    Migrate to pnpm

--- source/background/background.ts
+++ source/background/background.ts
@@ -1,5 +1,5 @@
 import * as browser from "webextension-polyfill";
-import isDevelopmentVersion from "./helpers/is-development-version";
+import isDevelopmentVersion from "../helpers/is-development-version";
 
 browser.runtime.onInstalled.addListener(({ reason }) => {
   // Only if the reason is explicitly "install"
