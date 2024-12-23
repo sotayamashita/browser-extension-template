8f03dbee2a810c903618cce03ac2d2a448292e92
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2024-12-23 14:34:59 +0900

    Setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: [
+      "distribution"
+    ],
+  },
+  {
+    languageOptions: { 
+      globals: {
+        ...globals.webextensions,
+        ...globals.browser,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+];
