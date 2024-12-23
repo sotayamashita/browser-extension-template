552576ff759d63fc3452eef913e8da64840a4640
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2024-12-23 14:42:34 +0900

    Fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,22 +2,19 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    ignores: [
-      "distribution"
-    ],
-  },
-  {
-    languageOptions: { 
-      globals: {
-        ...globals.webextensions,
-        ...globals.browser,
-      },
-    },
-  },
-  pluginJs.configs.recommended,
-  eslintConfigPrettier,
+	{
+		ignores: ["distribution"],
+	},
+	{
+		languageOptions: {
+			globals: {
+				...globals.webextensions,
+				...globals.browser,
+			},
+		},
+	},
+	pluginJs.configs.recommended,
+	eslintConfigPrettier,
 ];
