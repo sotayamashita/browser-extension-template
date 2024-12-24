fc825df5dd1ca3b771c30e194555b5c998a9d4f4
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2024-12-24 19:53:36 +0900

    Setup shadcn/ui and tailwind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,16 +3,21 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 import reactPlugin from "eslint-plugin-react";
+import tailwind from "eslint-plugin-tailwindcss";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	...tailwind.configs["flat/recommended"],
+	eslintConfigPrettier,
+
 	{
 		files: ["**/*.{js,mjs,cjs,ts}"],
-	},
-	{
-		ignores: ["distribution"],
-	},
-	{
+		plugins: {
+			// https://github.com/shadcn-ui/ui/issues/120
+			react: reactPlugin,
+		},
 		languageOptions: {
 			parserOptions: {
 				ecmaFeatures: {
@@ -30,8 +35,7 @@ export default [
 			},
 		},
 	},
-	pluginJs.configs.recommended,
-	...tseslint.configs.recommended,
-	reactPlugin.configs.flat.recommended,
-	eslintConfigPrettier,
+	{
+		ignores: ["distribution"],
+	},
 ];
